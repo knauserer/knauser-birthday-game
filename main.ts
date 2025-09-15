95953f61e33989411fb4994f1a8b6a8395489bdc
knauser <knauser@posteo.net>
2025-09-15 17:38:08 +0200

    Added basic game controls for moving and jumping.

--- main.ts
+++ main.ts
@@ -1,3 +1,14 @@
+controller.A.onEvent(ControllerButtonEvent.Pressed, function () {
+    hero.vy = -1 * Math.sqrt(32 * (2 * gravity))
+})
+let gravity = 0
+let hero: Sprite = null
 scene.setBackgroundColor(6)
-let hero = sprites.create(assets.image`monkey`, SpriteKind.Player)
-controller.moveSprite(hero)
+hero = sprites.create(assets.image`monkey`, SpriteKind.Player)
+scene.cameraFollowSprite(hero)
+tiles.setCurrentTilemap(tilemap`level1`)
+controller.moveSprite(hero, 100, 0)
+gravity = 350
+game.onUpdate(function () {
+    hero.ay = gravity
+})
---
+++ tilemap.g.ts
@@ -0,0 +1,39 @@
+// Auto-generated code. Do not edit.
+namespace myTiles {
+    //% fixedInstance jres blockIdentity=images._tile
+    export const transparency16 = image.ofBuffer(hex``);
+
+    helpers._registerFactory("tilemap", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+            case "level1":
+            case "level1":return tiles.createTilemap(hex`1000100000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000001030303030303030303030200000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000004040404040404040404040404040404`, img`
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . 2 2 2 2 2 2 2 2 2 2 2 2 . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 
+`, [myTiles.transparency16,sprites.builtin.forestTiles1,sprites.builtin.forestTiles3,sprites.builtin.forestTiles2,sprites.builtin.forestTiles0], TileScale.Sixteen);
+        }
+        return null;
+    })
+
+    helpers._registerFactory("tile", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+            case "transparency16":return transparency16;
+        }
+        return null;
+    })
+
+}
+// Auto-generated code. Do not edit.
