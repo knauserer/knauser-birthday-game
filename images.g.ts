a61768138e2f189a75a808e99267b66e38cbd5c7
knauser <knauser@posteo.net>
2025-09-17 15:30:24 +0200

    Added movement animations.

--- images.g.ts
+++ images.g.ts
@@ -28,7 +28,80 @@ c c c c c d d d e e f c . f e f
 
     helpers._registerFactory("animation", function(name: string) {
         switch(helpers.stringTrim(name)) {
-
+            case "walking left":
+            case "anim1":return [img`
+. . . . . . . f f f f f . . . . 
+. . . . . . f e e e e e f . . . 
+. . . . f f e e e d d d d f . . 
+. . . f d d e e d d d d d d c . 
+. . . c d b e e d f d d f d c . 
+f f . c d b e e d f d d f d d c 
+f e f . c f e e d d d d e e d c 
+f e f . . f e e e d c d d d d c 
+f e f . . f f e e e d c c c f . 
+f e f . f e e e e f f f f f . . 
+. f f f e e e e e e e f . . . . 
+. . f e e e e f e e f e f f . . 
+. . f e e e f f f e e f f e f . 
+. f b f f f f f f c d d b d d f 
+. f d d c f . . f d d d c d d f 
+. . f f f . . . f f f f f f f . 
+`, img`
+. . . . . . . f f f f f . . . . 
+. . . . . . f e e e e e f . . . 
+. . . . . f e e e d d d d f . . 
+. . . . f f e e d f d d f d c . 
+. . . f d d e e d f d d f d c . 
+. . . c d b e e d d d d e e d c 
+f f . c d b e e d d c d d d d c 
+f e f . c f e e d d d c c c c c 
+f e f . . f f e e d d d d d f . 
+f e f . f e e e e f f f f f . . 
+f e f f e e e e e e e f . . . . 
+. f f e e e e f e f f e f . . . 
+. . f e e e e f e f f e f . . . 
+. . . f e f f b d f b d f . . . 
+. . . f d b b d d c d d f . . . 
+. . . f f f f f f f f f . . . . 
+`];
+            case "facing left":
+            case "anim2":return [img`
+. . . . f f f f f . . . . . . . 
+. . . f e e e e e f . . . . . . 
+. . f d d d d e e e f . . . . . 
+. c d f d d f d e e f f . . . . 
+. c d f d d f d e e d d f . . . 
+c d e e d d d d e e b d c . . . 
+c d d d d c d d e e b d c . . . 
+c c c c c d d e e e f c . . . . 
+. f d d d d e e e f f . . . . . 
+. . f f f f f e e e e f . . . . 
+. . . . f f e e e e e e f . f f 
+. . . f e e f e e f e e f . e f 
+. . f e e f e e f e e e f . e f 
+. f b d f d b f b b f e f f e f 
+. f d d f d d f d d b e f f f f 
+. . f f f f f f f f f f f f f . 
+`];
+            case "facing right":
+            case "anim3":return [img`
+. . . . . . . f f f f f . . . . 
+. . . . . . f e e e e e f . . . 
+. . . . . f e e e d d d d f . . 
+. . . . f f e e d f d d f d c . 
+. . . f d d e e d f d d f d c . 
+. . . c d b e e d d d d e e d c 
+. . . c d b e e d d c d d d d c 
+. . . . c f e e e d d c c c c c 
+. . . . . f f e e e d d d d f . 
+. . . . f e e e e f f f f f . . 
+f f . f e e e e e e f f . . . . 
+f e . f e e f e e f e e f . . . 
+f e . f e e e f e e f e e f . . 
+f e f f e f b b f b d f d b f . 
+f f f f e b d d f d d f d d f . 
+. f f f f f f f f f f f f f . . 
+`];
         }
         return null;
     })
--- main.ts
+++ main.ts
@@ -1,14 +1,87 @@
-controller.A.onEvent(ControllerButtonEvent.Pressed, function () {
-    hero.vy = -1 * Math.sqrt(32 * (2 * gravity))
+scene.onHitWall(SpriteKind.Player, function (sprite, location) {
+    if (sprite.isHittingTile(CollisionDirection.Bottom)) {
+        numberOfJumpRemaining = numberOfJumps
+    }
 })
+controller.A.onEvent(ControllerButtonEvent.Pressed, function () {
+    doJump()
+})
+function doJump () {
+    if (numberOfJumpRemaining > 0) {
+        hero.vy = 0 - Math.sqrt(jumpHeight * (2 * gravity))
+        numberOfJumpRemaining += -1
+    }
+}
+let jumpHeight = 0
+let numberOfJumpRemaining = 0
+let numberOfJumps = 0
 let gravity = 0
 let hero: Sprite = null
 scene.setBackgroundColor(6)
 hero = sprites.create(assets.image`monkey`, SpriteKind.Player)
+character.loopFrames(
+hero,
+[img`
+    . . . . f f f f f . . . . . . . 
+    . . . f e e e e e f . . . . . . 
+    . . f d d d d e e e f f . . . . 
+    . c d d d d d d e e d d f . . . 
+    . c d f d d f d e e b d c . . . 
+    c d d f d d f d e e b d c . f f 
+    c d e e d d d d e e f c . f e f 
+    c d d d d c d e e e f . . f e f 
+    . f c c c d e e e f f . . f e f 
+    . . f f f f f e e e e f . f e f 
+    . . . . f e e e e e e e f f f . 
+    . . f f e f e e f e e e e f . . 
+    . f e f f e e f f f e e e f . . 
+    f d d b d d c f f f f f f b f . 
+    f d d c d d d f . . f c d d f . 
+    . f f f f f f f . . . f f f . . 
+    `,img`
+    . . . . f f f f f . . . . . . . 
+    . . . f e e e e e f . . . . . . 
+    . . f d d d d e e e f . . . . . 
+    . c d f d d f d e e f f . . . . 
+    . c d f d d f d e e d d f . . . 
+    c d e e d d d d e e b d c . . . 
+    c d d d d c d d e e b d c . f f 
+    c c c c c d d d e e f c . f e f 
+    . f d d d d d e e f f . . f e f 
+    . . f f f f f e e e e f . f e f 
+    . . . . f e e e e e e e f f e f 
+    . . . f e f f e f e e e e f f . 
+    . . . f e f f e f e e e e f . . 
+    . . . f d b f d b f f e f . . . 
+    . . . f d d c d d b b d f . . . 
+    . . . . f f f f f f f f f . . . 
+    `],
+200,
+character.rule(Predicate.MovingLeft, Predicate.HittingWallDown)
+)
+character.loopFrames(
+hero,
+assets.animation`walking left`,
+200,
+character.rule(Predicate.MovingRight, Predicate.HittingWallDown)
+)
+character.loopFrames(
+hero,
+assets.animation`facing left`,
+200,
+character.rule(Predicate.FacingLeft, Predicate.HittingWallDown, Predicate.NotMoving)
+)
+character.loopFrames(
+hero,
+assets.animation`facing right`,
+200,
+character.rule(Predicate.FacingRight, Predicate.HittingWallDown, Predicate.NotMoving)
+)
 scene.cameraFollowSprite(hero)
-tiles.setCurrentTilemap(tilemap`level1`)
+tiles.setCurrentTilemap(tilemap`level0`)
 controller.moveSprite(hero, 100, 0)
 gravity = 350
-game.onUpdate(function () {
-    hero.ay = gravity
-})
+hero.ay = gravity
+numberOfJumps = 2
+numberOfJumpRemaining = numberOfJumps
+jumpHeight = 32
